7c30b8a3f7b17f55f5e4745966c3316f35fd2724
David Hasek <hasek.david1@gmail.com>
2024-04-20 01:43:14 +0200

    Move frontend into src subfolder

---
+++ frontend/src/scripts/extractTranslationIds.ts
@@ -0,0 +1,32 @@
+import { exec } from 'child_process';
+import { readFile, writeFile } from 'fs/promises';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
+
+const enLocaleFile = `${__dirname}/../src/utils/locales/en.json`;
+
+const main = async () => {
+	await execAsync(`yarn formatjs extract 'src/**/*.ts*' --ignore='**/*.d.ts' --out-file ${process.env.TMPDIR}/ood/ids.json`);
+
+	const [idsContent, originalContent] = await Promise.all([
+		readFile(`${process.env.TMPDIR}/ood/ids.json`, 'utf8'),
+		readFile(enLocaleFile, 'utf8'),
+	]);
+
+	const idsData = JSON.parse(idsContent);
+	const originalData = JSON.parse(originalContent);
+
+	const ids = Object.keys(idsData).sort();
+
+	const resultData = Object.fromEntries(
+		ids.map((id) => {
+			return [id, originalData[id] ?? ''];
+		}),
+	);
+	const resultContent = JSON.stringify(resultData, null, '\t');
+
+	await writeFile(enLocaleFile, `${resultContent}\n`, 'utf8');
+};
+
+main().catch(console.error);
