18d4334032b2660798abce8f5a397891f850c5dc
David Hasek <hasek.david1@gmail.com>
2024-05-21 01:09:38 +0200

    Add suspens for usesearch hook

--- frontend2/src/app/properties/layout.tsx
+++ frontend2/src/app/properties/layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from '@mui/material';
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, Suspense } from 'react';
 import { FiltersPanel } from '../../components/properties/Filters/FiltersPanel';
 
 const PropertiesLayout: FC<
@@ -9,9 +9,11 @@ const PropertiesLayout: FC<
 > = ({ children }) => {
 	return (
 		<Stack direction={'row'} minHeight={'100vh'}>
-			<Stack display={{ lg: 'inherit', xs: 'none' }}>
-				<FiltersPanel />
-			</Stack>
+			<Suspense>
+				<Stack display={{ lg: 'inherit', xs: 'none' }}>
+					<FiltersPanel />
+				</Stack>
+			</Suspense>
 			{children}
 		</Stack>
 	);
