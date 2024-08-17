96501833aed6d7fe4f88c35250dd4e4ece9ad0ba
David Hasek <hasek.david1@gmail.com>
2024-08-17 11:32:34 +0200

    App like setup

---
+++ frontend/src/app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Andalusia Home',
+    short_name: 'AH',
+    description: 'Spanish real estates for sale',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '/frontend/public/favicon.ico',
+        sizes: '16x16',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
