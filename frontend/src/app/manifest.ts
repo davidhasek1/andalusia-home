5edf15a62463c32641f87837ac35b2ee3b678013
David Hasek <hasek.david1@gmail.com>
2024-08-17 11:42:50 +0200

    App Icon

--- frontend/src/app/manifest.ts
+++ frontend/src/app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#fff',
     icons: [
       {
-        src: '/frontend/public/favicon.ico',
+        src: '/favicon.ico',
         sizes: '16x16',
         type: 'image/x-icon',
       },
