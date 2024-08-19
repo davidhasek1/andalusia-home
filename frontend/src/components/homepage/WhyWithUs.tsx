d24a0570e3f5932fcf352155f8e0dc30b2b381cd
David Hasek <hasek.david1@gmail.com>
2024-08-19 14:35:27 +0200

    Refactor cards

--- frontend/src/components/homepage/WhyWithUs.tsx
+++ frontend/src/components/homepage/WhyWithUs.tsx
@@ -9,6 +9,29 @@ import theme from '../../theme';
 
 type Props = CommonProps;
 
+const data = [
+	{
+		id: 1,
+		title: <FormattedMessage id={'homepage.section.why-with-us.card1.title'} />,
+		description: <FormattedMessage id={'homepage.section.why-with-us.card1.description'} />,
+	},
+	{
+		id: 2,
+		title: <FormattedMessage id={'homepage.section.why-with-us.card2.title'} />,
+		description: <FormattedMessage id={'homepage.section.why-with-us.card2.description'} />,
+	},
+	{
+		id: 3,
+		title: <FormattedMessage id={'homepage.section.why-with-us.card3.title'} />,
+		description: <FormattedMessage id={'homepage.section.why-with-us.card3.description'} />,
+	},
+	{
+		id: 4,
+		title: <FormattedMessage id={'homepage.section.why-with-us.card4.title'} />,
+		description: <FormattedMessage id={'homepage.section.why-with-us.card4.description'} />,
+	},
+];
+
 export const WhyWithUs: FC<Props> = ({ order }) => {
 	return (
 		<Stack gap={2}>
@@ -22,74 +45,25 @@ export const WhyWithUs: FC<Props> = ({ order }) => {
 			</Typography>
 
 			<Grid container spacing={4} sx={{ maxWidth: '85%', margin: 'auto' }}>
-				<Grid lg={6} xs={12} sx={{ textAlign: 'center' }} item>
-					<Stack
-						direction={'column'}
-						height={'100%'}
-						sx={{
-							border: `1px solid ${theme.palette.grey[300]}`,
-							p: 3,
-						}}
-					>
-						<Typography variant={'h5'} fontWeight={900} mb={2}>
-							<FormattedMessage id={'homepage.section.why-with-us.card1.title'} />
-						</Typography>
-						<Typography variant={'caption'} fontSize={16}>
-							<FormattedMessage id={'homepage.section.why-with-us.card1.description'} />
-						</Typography>
-					</Stack>
-				</Grid>
-				<Grid lg={6} xs={12} sx={{ textAlign: 'center' }} item>
-					<Stack
-						direction={'column'}
-						height={'100%'}
-						sx={{
-							border: `1px solid ${theme.palette.grey[300]}`,
-							p: 3,
-						}}
-					>
-						<Typography variant={'h5'} fontWeight={900} mb={2}>
-							<FormattedMessage id={'homepage.section.why-with-us.card2.title'} />
-						</Typography>
-						<Typography variant={'caption'} fontSize={16}>
-							<FormattedMessage id={'homepage.section.why-with-us.card2.description'} />
-						</Typography>
-					</Stack>
-				</Grid>
-				<Grid lg={6} xs={12} sx={{ textAlign: 'center' }} item>
-					<Stack
-						direction={'column'}
-						height={'100%'}
-						sx={{
-							border: `1px solid ${theme.palette.grey[300]}`,
-							p: 3,
-						}}
-					>
-						<Typography variant={'h5'} fontWeight={900} mb={2}>
-							<FormattedMessage id={'homepage.section.why-with-us.card3.title'} />
-						</Typography>
-						<Typography variant={'caption'} fontSize={16}>
-							<FormattedMessage id={'homepage.section.why-with-us.card3.description'} />
-						</Typography>
-					</Stack>
-				</Grid>
-				<Grid lg={6} xs={12} sx={{ textAlign: 'center' }} item>
-					<Stack
-						direction={'column'}
-						height={'100%'}
-						sx={{
-							border: `1px solid ${theme.palette.grey[300]}`,
-							p: 3,
-						}}
-					>
-						<Typography variant={'h5'} fontWeight={900} mb={2}>
-							<FormattedMessage id={'homepage.section.why-with-us.card4.title'} />
-						</Typography>
-						<Typography variant={'caption'} fontSize={16}>
-							<FormattedMessage id={'homepage.section.why-with-us.card4.description'} />
-						</Typography>
-					</Stack>
-				</Grid>
+				{data.map((item) => (
+					<Grid key={item.id} lg={6} xs={12} sx={{ textAlign: 'center' }} item>
+						<Stack
+							direction={'column'}
+							height={'100%'}
+							sx={{
+								border: `1px solid ${theme.palette.grey[300]}`,
+								p: 3,
+							}}
+						>
+							<Typography variant={'h5'} fontWeight={900} mb={2}>
+								{item.title}
+							</Typography>
+							<Typography variant={'caption'} fontSize={16}>
+								{item.description}
+							</Typography>
+						</Stack>
+					</Grid>
+				))}
 			</Grid>
 		</Stack>
 	);
