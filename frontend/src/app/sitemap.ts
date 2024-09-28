b063dc6ce07cff01885befc87347e391d3d6b290
Daniel <hasek.dan1@gmail.com>
2024-09-28 13:05:20 +0200

    feat: seo

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -1,26 +1,34 @@
+import { BlogPost } from 'components/blog/BlogCard';
 import { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const BASE_URL = 'https://andalusiahome.cz';
+
+	const res = await fetch('https://api.npoint.io/9b28ba3416db05aea576');
+	const blogsData = await res.json();
+
+	const blogs = blogsData.blogPosts.map((item: BlogPost) => ({
+		url: `${BASE_URL}/blog/${item.slug}`,
+		changeFrequency: 'weekly',
+		priority: 1,
+	}));
+
 	return [
 		{
 			url: 'https://www.andalusiahome.cz/',
-			lastModified: new Date(),
 		},
 		{
 			url: 'https://www.andalusiahome.cz/about-us',
-			lastModified: new Date(),
 		},
 		{
 			url: 'https://www.andalusiahome.cz/contact',
-			lastModified: new Date(),
 		},
 		{
 			url: 'https://www.andalusiahome.cz/properties',
-			lastModified: new Date(),
 		},
 		{
 			url: 'https://www.andalusiahome.cz/blog',
-			lastModified: new Date(),
 		},
+		...blogs,
 	];
 }
--- frontend/src/contexts/ApolloClientContext.tsx
+++ frontend/src/contexts/ApolloClientContext.tsx
@@ -1,20 +1,7 @@
 'use client';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+import { client } from 'lib/apolloClient';
 import { FC, PropsWithChildren } from 'react';
-
-let GRAPHQL_URL;
-
-if (process.env.NODE_ENV === 'development') {
-	console.log('using local');
-	GRAPHQL_URL = 'http://localhost:4000/graphql';
-} else {
-	console.log('using render');
-	GRAPHQL_URL = 'https://andalusia-home-api.onrender.com/graphql';
-}
-const client = new ApolloClient({
-	uri: GRAPHQL_URL,
-	cache: new InMemoryCache(),
-});
 
 const ApolloClientContext: FC<PropsWithChildren> = ({ children }) => {
 	return <ApolloProvider client={client}>{children}</ApolloProvider>;
